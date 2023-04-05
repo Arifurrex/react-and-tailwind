22274d6ef9a7530e7c5539bda9d42d816ce7f344
arifur rahman <arifurrex@gmail.com>
2023-04-06 02:44:17 +0600

    vanillajs2

--- increment.js
+++ increment.js
@@ -6,3 +6,13 @@ button.addEventListener('click', function () {
     number++;
     display.textContent = number;
 })
+
+// for button 2
+const button2 = document.getElementById('button2');
+const display2 = document.getElementById('display2')
+let number2 = 0;
+
+button2.addEventListener('click', function () {
+    number2++;
+    display2.textContent = number2;
+})
